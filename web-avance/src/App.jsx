fda0a45e11415e095cb88607cc667c4ff4ef8ac0
Marevan Conti <marevanconti@gmail.com>
2023-05-04 19:06:06 +0200

    Private Route test

--- web-avance/src/App.jsx
+++ web-avance/src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
 
 import Accueil from "./screens/Accueil";
 import Login from "./screens/Login";
@@ -6,7 +7,7 @@ import Login from "./screens/Login";
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Accueil />} />
+      <Route exact path="/" element={<PrivateRoute page={<Accueil />} />} />
       <Route path="/login" element={<Login />} />
     </Routes>
   );
